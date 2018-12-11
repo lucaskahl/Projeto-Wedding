d5fff12598d68f3d1bd92efb5fc842ac381dd79b
Lucas <lucaschangkahl@gmail.com>
2018-12-11 16:20:33 -0200

    Feito logica do contador, necessario estilo

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
-let year = document.querySelector('.year');
-let month = document.querySelector('.month');
-let week = document.querySelector('.week');
-let seconds = document.querySelector('.seconds');
 
-let casamentoDate = '10/05/2022';
+/* Tempo Casamento */
+
+var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
+
+var x = setInterval(() => {
+  
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "expirado";
+  }
+
+
+}, 1000);
+
