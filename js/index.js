36eef50c011d15a5ae9ce79cd66897e2f824a387
Lucas <lucaschangkahl@gmail.com>
2018-12-11 08:22:33 -0200

    Feito base do timer e criado arquivo JS.

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let year = document.querySelector('.year');
+let month = document.querySelector('.month');
+let week = document.querySelector('.week');
+let seconds = document.querySelector('.seconds');
+
+let casamentoDate = '10/05/2022';
